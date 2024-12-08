719ea7d87ce1d8ceb7283e44833ea4bf7fe8fa62
Nursultan <ernazarovazargul@gmail.com>
2024-12-08 11:26:50 +0500

    NOne

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-// main.tsx
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { ReactDOM } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { App } from "./App";
 import { AuthProvider } from "./authContext"; // Импорт контекста
